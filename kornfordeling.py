aa17723cee8f61214d9c288b92c789f23b8ea0b9
Ola Haugen Havrevoll <havrevol@ntnu.no>
2021-08-12 10:17:45 +0200

    Har gjort ein del på kornfordelinga

--- kornfordeling.py
+++ kornfordeling.py
@@ -7,21 +7,55 @@ Created on Sat Jul 10 21:24:39 2021
 import random
 import numpy as np
 from scipy import interpolate
+from math import log2, pi
+
+rho = 2650.0 / 1e6
+
+PSD = np.array([[0.01,  0.00], #Det er ukjend kor 0 % er, altså kva som er minste storleik. Må anta at det er ganske lite.
+                [0.149, 0.02],	
+                [0.297, 0.095],	
+                [0.594, 0.28],	
+                [1.188, 0.55],	
+                [2.376, 0.79],	
+                [4.752, 0.91],	
+                [9.504, 0.98],	
+                [12.0,  1.00]])
+
+# Skal laga kode for å utvida tabellen med 1000 element mellom kvar verdi.
+
+element = 1000
+
+PSD_full = np.zeros((element*(len(PSD)-1)+1, 4))
+
+for i in range(len(PSD)-1):
+    # PSD_full[i*element,1] = PSD[i,1]
+    
+    diff = PSD[i+1,1]- PSD[i,1]
+    
+    for j in range(element):
+        PSD_full[i*element + j, 1] = PSD[i,1]+ diff * j / element
+        
+    PSD_full[i*element, 0] = PSD[i, 0]
+    
+    for j in range(element):
+        PSD_full[i*element + j, 0] = 2**(log2(PSD[i,0]) + (log2(PSD[i+1,0]) - log2(PSD[i,0])) * (PSD_full[i*element + j, 1] - PSD[i,1]) / (PSD[i+1,1] - PSD[i,1]))
+        
+        
+
+PSD_full[-1,0] = PSD[-1,0]
+PSD_full[-1,1] = PSD[-1,1]
+
+tal_per_g = np.diff(PSD_full[:,1])/(pi/6 * rho * PSD_full[1:,0])
+PSD_full[:,2] = 
+
+PSD_full = PSD_full.T
+PSD = PSD.T
 
 
-PSD = np.array([[0.1,0],
-[0.149,0.02],	
-[0.297,0.095],	
-[0.594,0.28],	
-[1.188,0.55],	
-[2.376,0.79],	
-[4.752,0.91],	
-[9.504,0.98],	
-[12.0,1]]).T
 
 f = interpolate.interp1d(PSD[1], PSD[0])
 
-tal = int(1e9)
+tal = int(5)
 
 # np.random.seed()
 a = np.random.uniform(0.0, 1.0, size = tal)
@@ -44,7 +78,8 @@ x1 = np.cumsum(x[0])
 # y3 = 6*x + 10
 myDPI = 300
 fig, ax = plt.subplots(figsize=(1190/myDPI,800/myDPI),dpi=myDPI)
-ax.semilogx(bins, x1, color="blue", label="kornfordeling")
+# ax.semilogx(bins, x1, color="blue", label="kornfordeling")
+ax.semilogx(PSD_full[0], PSD_full[1], color="red", label="original")
 # ax.set_xticks(bins)
 # ax.set_xticklabels(bins)
 # ax.plot(x, y2, color="red", label="y'(x)")
