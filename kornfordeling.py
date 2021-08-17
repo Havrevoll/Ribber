51143b67f799f84aeca0cd7e9cfbbdefeca99d3e
Ola Haugen Havrevoll <havrevoll@gmail.com>
2021-08-17 12:23:21 +0200

    Har laga lift-funksjon og prøvd på multithreading, men det gjekk ikkje så bra

--- kornfordeling.py
+++ kornfordeling.py
@@ -8,10 +8,14 @@ import random
 import numpy as np
 from scipy import interpolate
 from math import log2, pi
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"] = "STIXGeneral"
+plt.rcParams['mathtext.fontset'] = 'stix'
 
 rho = 2650.0 / 1e6
 
-PSD = np.array([[0.01,  0.00], #Det er ukjend kor 0 % er, altså kva som er minste storleik. Må anta at det er ganske lite.
+PSD = np.array([[0.05,  0.00], #Det er ukjend kor 0 % er, altså kva som er minste storleik. Må anta at det er ganske lite.
                 [0.149, 0.02],	
                 [0.297, 0.095],	
                 [0.594, 0.28],	
@@ -45,12 +49,15 @@ for i in range(len(PSD)-1):
 PSD_full[-1,0] = PSD[-1,0]
 PSD_full[-1,1] = PSD[-1,1]
 
-tal_per_g = np.diff(PSD_full[:,1])/(pi/6 * rho * PSD_full[1:,0])
-PSD_full[:,2] = 
+PSD_full[:,2] = np.concatenate(([0],np.diff(PSD_full[:,1])/(pi/6 * rho * PSD_full[1:,0]**3)))
+
+sum_partiklar = np.sum(PSD_full[:,2])
+
+PSD_full[:,3] = np.cumsum(PSD_full[:,2])/sum_partiklar
+
 
 PSD_full = PSD_full.T
 PSD = PSD.T
-
 
 
 f = interpolate.interp1d(PSD[1], PSD[0])
@@ -64,13 +71,12 @@ b = np.random.uniform(0.0, 88.0, size = tal)
 c = np.random.uniform(0.0, 3.0, size = tal)
 d = np.vstack((korndiameter,b,c)).T
 
-import matplotlib.pyplot as plt
 
-bins = 1.20**(np.arange(-4,18))
-print( "bins: ", bins)
+# bins = 1.20**(np.arange(-4,18))
+# print( "bins: ", bins)
 
-x = np.histogram(korndiameter,np.concatenate(([0],bins)))
-x1 = np.cumsum(x[0])
+# x = np.histogram(korndiameter,np.concatenate(([0],bins)))
+# x1 = np.cumsum(x[0])
 
 # x = np.linspace(-5, 2, 100)
 # y1 = x**3 + 5*x**2 + 10
@@ -79,13 +85,13 @@ x1 = np.cumsum(x[0])
 myDPI = 300
 fig, ax = plt.subplots(figsize=(1190/myDPI,800/myDPI),dpi=myDPI)
 # ax.semilogx(bins, x1, color="blue", label="kornfordeling")
-ax.semilogx(PSD_full[0], PSD_full[1], color="red", label="original")
+ax.semilogx(PSD_full[0], PSD_full[3], color="red", label="original")
 # ax.set_xticks(bins)
 # ax.set_xticklabels(bins)
 # ax.plot(x, y2, color="red", label="y'(x)")
 # ax.plot(x, y3, color="green", label="y”(x)")
-# ax.set_xlabel("x")
-# ax.set_ylabel("y")
+ax.set_xlabel("d [mm]")
+ax.set_ylabel("% tal partiklar passert")
 # ax.legend()
 
 # logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
